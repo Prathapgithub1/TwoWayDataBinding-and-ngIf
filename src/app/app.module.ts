486fb920e1b152a603fef3321756ff72e120bdb6
rajukunarapu <rajukunarapu49@gmail.com>
2024-11-23 11:34:21 +0530

    ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import{FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StructuralDirectivesComponent
+    StructuralDirectivesComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
